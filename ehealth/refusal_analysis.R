3089061784df350a18d0a911c90e6711ca36d4a7
King <king.wa.tam@gmail.com>
2021-06-30 10:04:53 +0800

    Load latest WBS refusal & unreachable data, & reanalyse group differences with completed data

--- ehealth/refusal_analysis.R
+++ ehealth/refusal_analysis.R
@@ -10,96 +10,96 @@ library(labelled)
 
 Sys.setlocale(locale =  "cht") # set locale to traditional Chinese
 setwd(sprintf("~%s/ehealth/wbs", setpath))
-# # merge datasets ----
-# df <- xlsx::read.xlsx2("EHealth_NEW_DATA_WBS_Complete_SCHSA_2021-06-05.xlsx", sheetName  = "Raw data"
-#                        , encoding = "UTF-8"
-#                        , header = TRUE
+# merge datasets ----
+df <- xlsx::read.xlsx2("EHealth_NEW_DATA_WBS_Complete_SCHSA_2021-06-05.xlsx", sheetName  = "Raw data"
+                       , encoding = "UTF-8"
+                       , header = TRUE
+)
+names(df)[names(df)=="Uid"] <- "member_id"
+
+setwd(sprintf("~%s/ehealth/wbs", setpath))
+# get 18 district info for each centre
+temp <- xlsx::read.xlsx2("Result_18districtQ1 (2021-01-31).xlsx", sheetName  = "District"
+                           , encoding = "UTF-8"
+                           , header = TRUE
+)
+names(temp)[names(temp)=="Centre.code"] <- "Survey_centre"
+names(temp)[names(temp)=="X18.district"] <- "district_cht"
+
+df <- merge(df, temp[, c("Survey_centre", "district_cht")], # extract item matched by case ID
+            by=c("Survey_centre"), all.x = TRUE)
+
+# merge refusal and cannot contact data
+temp <- xlsx::read.xlsx2("EHealthII_baseline_reject list 20120623.xlsx", sheetName  = "reject"
+                         , encoding = "UTF-8"
+                         , header = TRUE
+)
+temp <- temp[1:159,] # remove extra rows
+names(temp)[1] <- "member_id"
+temp <- temp[, c(1, 7:8)]
+temp$type <- "refused"
+
+
+temp2 <- xlsx::read.xlsx2("EHealthII_baseline_cannot contact list 20120623.xlsx", sheetName  = "cannot contact"
+                         , encoding = "UTF-8"
+                         , header = TRUE
+)
+temp2 <- temp2[1:82,] # remove extra rows
+names(temp2)[1] <- "member_id"
+temp2 <- temp2[, c(1, 7:8)]
+temp2$type <- "unreachable"
+
+temp <- rbind(temp, temp2)
+rm(temp2)
+
+df <- merge(df, temp[, c("member_id", "reason_grouped", "round", "type")], # extract item matched by case ID
+            by=c("member_id"), all.x = TRUE)
+
+# df <-  merge(temp, df, # extract item matched by case ID
+#                     by=c("Uid"), all.x = TRUE)
+
+# # legacy code
+# # merge 1st refusal data
+# temp <- xlsx::read.xlsx2("WBS result of rejected and incomplete baseline 20210331.xlsx", sheetName  = "wbs 1st"
+#                             , encoding = "UTF-8"
+#                             , header = TRUE
 # )
-# names(df)[names(df)=="Uid"] <- "member_id"
-# 
-# setwd(sprintf("~%s/ehealth/wbs", setpath))
-# # get 18 district info for each centre
-# temp <- xlsx::read.xlsx2("Result_18districtQ1 (2021-01-31).xlsx", sheetName  = "District"
-#                            , encoding = "UTF-8"
-#                            , header = TRUE
+# temp <- temp[1:318,] # remove extra rows
+# temp$refused <- 1
+# df <- merge(df, temp[, c("Uid", "refused")], # extract item matched by case ID
+#             by=c("Uid"), all.x = TRUE)
+# rm(temp)
+# df$refused <- ifelse(is.na(df$refused), 0, df$refused)
+#
+# # merge 2nd refusal data
+# temp2 <- xlsx::read.xlsx2("WBS result of rejected and incomplete baseline 20210331.xlsx", sheetName  = "wbs 2nd"
+#                           , encoding = "UTF-8"
+#                           , header = TRUE
 # )
-# names(temp)[names(temp)=="Centre.code"] <- "Survey_centre"
-# names(temp)[names(temp)=="X18.district"] <- "district_cht"
-# 
-# df <- merge(df, temp[, c("Survey_centre", "district_cht")], # extract item matched by case ID
-#             by=c("Survey_centre"), all.x = TRUE)
-# 
-# # merge refusal and cannot contact data
-# temp <- xlsx::read.xlsx2("EHealthII_baseline_reject list 20120623.xlsx", sheetName  = "reject"
-#                          , encoding = "UTF-8"
-#                          , header = TRUE
-# )
-# temp <- temp[1:159,] # remove extra rows
-# names(temp)[1] <- "member_id"
-# temp <- temp[, c(1, 7:8)]
-# temp$type <- "refused"
-# 
-# 
-# temp2 <- xlsx::read.xlsx2("EHealthII_baseline_cannot contact list 20120623.xlsx", sheetName  = "cannot contact"
-#                          , encoding = "UTF-8"
-#                          , header = TRUE
-# )
-# temp2 <- temp2[1:82,] # remove extra rows
-# names(temp2)[1] <- "member_id"
-# temp2 <- temp2[, c(1, 7:8)]
-# temp2$type <- "unreachable"
-# 
-# temp <- rbind(temp, temp2)
+#
+# temp2 <- temp2[1:19,] # remove extra rows
+# temp2$refused2 <- 1
+# df <- merge(df, temp2[, c("Uid", "refused2")], # extract item matched by case ID
+#             by=c("Uid"), all.x = TRUE)
 # rm(temp2)
-# 
-# df <- merge(df, temp[, c("member_id", "reason_grouped", "round", "type")], # extract item matched by case ID
-#             by=c("member_id"), all.x = TRUE)
-# 
-# # df <-  merge(temp, df, # extract item matched by case ID
-# #                     by=c("Uid"), all.x = TRUE)
-# 
-# # # legacy code
-# # # merge 1st refusal data
-# # temp <- xlsx::read.xlsx2("WBS result of rejected and incomplete baseline 20210331.xlsx", sheetName  = "wbs 1st"
-# #                             , encoding = "UTF-8"
-# #                             , header = TRUE
-# # )
-# # temp <- temp[1:318,] # remove extra rows
-# # temp$refused <- 1
-# # df <- merge(df, temp[, c("Uid", "refused")], # extract item matched by case ID
-# #             by=c("Uid"), all.x = TRUE)
-# # rm(temp)
-# # df$refused <- ifelse(is.na(df$refused), 0, df$refused)
-# #
-# # # merge 2nd refusal data
-# # temp2 <- xlsx::read.xlsx2("WBS result of rejected and incomplete baseline 20210331.xlsx", sheetName  = "wbs 2nd"
-# #                           , encoding = "UTF-8"
-# #                           , header = TRUE
-# # )
-# #
-# # temp2 <- temp2[1:19,] # remove extra rows
-# # temp2$refused2 <- 1
-# # df <- merge(df, temp2[, c("Uid", "refused2")], # extract item matched by case ID
-# #             by=c("Uid"), all.x = TRUE)
-# # rm(temp2)
-# # df$refused2 <- ifelse(is.na(df$refused2), 0, df$refused2)
-# #
-# # temp <- xlsx::read.xlsx2("WBS result of rejected and incomplete baseline 20210331.xlsx", sheetName  = "round 1- 5"
-# #                          , encoding = "UTF-8"
-# #                          , header = TRUE
-# # )
-# # temp <- temp[1:337,] # remove extra rows
-# # names(temp)[names(temp)=="UID"] <- "Uid"
-# # df <- merge(df, temp[, c("Uid", "remark")], # extract item matched by case ID
-# #             by=c("Uid"), all.x = TRUE)
-# # rm(temp)
-# 
-# df$Survey_date <- as.Date(as.numeric(df$Survey_date), origin = "1899-12-30")
-# df$Birth_date <- as.Date(as.numeric(df$Birth_date), origin = "1899-12-30")
-# names(df)[names(df)=="X.U.4E2D..U.5FC3."] <- "centre_cht"
-# names(df)[names(df)=="X.U.6A5F..U.69CB."] <- "ngo_cht"
-# 
-# saveRDS(df, file = "refusal_data.rds")
+# df$refused2 <- ifelse(is.na(df$refused2), 0, df$refused2)
+#
+# temp <- xlsx::read.xlsx2("WBS result of rejected and incomplete baseline 20210331.xlsx", sheetName  = "round 1- 5"
+#                          , encoding = "UTF-8"
+#                          , header = TRUE
+# )
+# temp <- temp[1:337,] # remove extra rows
+# names(temp)[names(temp)=="UID"] <- "Uid"
+# df <- merge(df, temp[, c("Uid", "remark")], # extract item matched by case ID
+#             by=c("Uid"), all.x = TRUE)
+# rm(temp)
+
+df$Survey_date <- as.Date(as.numeric(df$Survey_date), origin = "1899-12-30")
+df$Birth_date <- as.Date(as.numeric(df$Birth_date), origin = "1899-12-30")
+names(df)[names(df)=="X.U.4E2D..U.5FC3."] <- "centre_cht"
+names(df)[names(df)=="X.U.6A5F..U.69CB."] <- "ngo_cht"
+
+saveRDS(df, file = "refusal_data.rds")
 
 # load data ----
 df <- readRDS("refusal_data.rds") 
