efb4f7b7be90e3b56d838e2835e109b30679648c
King <king.wa.tam@gmail.com>
2021-06-30 09:15:25 +0800

    Use latest high-risk WBS data for age info

--- ehealth/ehealth_cleaning.R
+++ ehealth/ehealth_cleaning.R
@@ -12,7 +12,7 @@ library(ggplot2)
 
 Sys.setlocale(locale =  "cht") # set locale to traditional Chinese
 setwd(sprintf("~%s/ehealth", setpath))
-df <- foreign_to_labelled(haven::read_sav("EHealthIIEvaluation_DATA_NOHDRS_2021-06-22_1135.sav", encoding = "UTF-8")) # Sys.setlocale(category = "LC_ALL", locale = "cht")
+df <- foreign_to_labelled(haven::read_sav("EHealthIIEvaluation_DATA_NOHDRS_2021-06-29_1115.sav", encoding = "UTF-8")) # Sys.setlocale(category = "LC_ALL", locale = "cht")
 
 # # load Eva's data
 # XLConnect::xlcFreeMemory() # rtools is also required to be installed to avoid error
@@ -24,7 +24,7 @@ df <- foreign_to_labelled(haven::read_sav("EHealthIIEvaluation_DATA_NOHDRS_2021-
 # names(df2) <- c(names(df), names(df2)[87:96])
 
 # survey data cleaning ----
-temp <- xlsx::read.xlsx2("Raw Data (from Oct 8) and Summary 20210622_duplicates.xlsx", sheetName  = "duplicate record"
+temp <- xlsx::read.xlsx2("Raw Data (from Oct 8) and Summary 20210628_duplicates.xlsx", sheetName  = "duplicate record"
                          , encoding = "UTF-8"
                          , header = TRUE
 )
@@ -37,7 +37,8 @@ rm(temp)
 df <- df[!(df$record_id %in% dupes),] # remove duplicates
 
 # hardcode missing timestamps 
-df$ehealth_eval_timestamp[df$record_id %in% df$record_id[df$ehealth_eval_timestamp %in% ""]] <-
+# df$record_id[df$ehealth_eval_timestamp %in% ""] # show record IDs with missing timestamps
+df$ehealth_eval_timestamp[df$record_id %in% c("846", "852", "857", "872")] <-
   c("2020-10-15 15:01:00", "2020-10-15 15:45:00", "2020-10-15 16:20:00", "2020-10-16 10:00:00")
 
 # fix problematic member IDs
@@ -47,7 +48,7 @@ df$member_id[grepl("\t", df$member_id, ignore.case = TRUE)] <-
 
 df$ehealth_eval_timestamp <- lubridate::ymd_hms(df$ehealth_eval_timestamp, tz = "Asia/Hong_Kong", quiet =  TRUE)
 
-# # examine duplicates
+# # check duplicates
 # wbs %>%
 #   add_count(member_id) %>%
 #   filter(n >= 2) %>% View()
@@ -89,13 +90,16 @@ df$ehealth_eval_timestamp <- lubridate::ymd_hms(df$ehealth_eval_timestamp, tz = 
 
 # df <- cbind(df, scoring(df))
   
-# reverse EQ5D items
-df[eq5d_[1:5]] <-  (df[eq5d_[1:5]]-6)*-1
-
-df$eq5d_score <- ifelse(is.na(df$eq5d_mobility) | is.na(df$eq5d_self_care) | is.na(df$eq5d_usual_activity) | 
-                          is.na(df$eq5d_pain_discomfort) | is.na(df$eq5d_anxiety_depression),
+# score EQ-5D-5L
+score_EQ5D <- function(df){
+  df[eq5d_[1:5]] <-  (df[eq5d_[1:5]]-6)*-1 # reverse EQ5D items
+  df$eq5d_score <- ifelse(is.na(df$eq5d_mobility) | is.na(df$eq5d_self_care) | is.na(df$eq5d_usual_activity) | 
+                            is.na(df$eq5d_pain_discomfort) | is.na(df$eq5d_anxiety_depression),
                           NA, paste0(df$eq5d_mobility, df$eq5d_self_care, df$eq5d_usual_activity, df$eq5d_pain_discomfort, df$eq5d_anxiety_depression))
-df$eq5d =  eq5d_fast(scores=df$eq5d_score, country="HongKong", version="5L", type="VT", ignore.invalid = TRUE)
+  df$eq5d =  eq5d_fast(scores=df$eq5d_score, country="HongKong", version="5L", type="VT", ignore.invalid = TRUE)
+  return(subset(df, select = c(eq5d_score, eq5d)))
+}
+df <- cbind(df, score_EQ5D(df))
 
 df$amic <- ifelse(df$amic_sum >= 3, 1, 0)
 val_labels(df$amic) <- c("AMIC >= 3" = 1, "AMIC < 3" = 0)
@@ -106,9 +110,13 @@ val_labels(df$pase_c_12_1) <- c(Yes=1, No=0)
 setwd(sprintf("~%s/ehealth/JCreport 20210409/data/Overall", setpath))
 wbs <- foreign_to_labelled(haven::read_sav("EHealth_NEW_DATA_WBS_Complete_2021-01-31_Overall.sav", encoding = "UTF-8")) # Sys.setlocale(category = "LC_ALL", locale = "cht")
 
-setwd(sprintf("~%s/ehealth/fwweeklyehealthreport_20210515", setpath))
-wbs2 <- xlsx::read.xlsx2("EHealth_NEW_DATA_WBS_Complete_SCHSA_2021-05-15.xlsx", sheetName  = "Raw data"
-                         )
+setwd(sprintf("~%s/ehealth/wbs", setpath))
+wbs2 <- xlsx::read.xlsx2("EHealth_NEW_DATA_WBS_Complete_SCHSA_2021-06-05.xlsx", sheetName  = "Raw data"
+                         ) # latest WBS high-risk data
+wbs2$Survey_date <- as.Date(as.numeric(wbs2$Survey_date), origin = "1899-12-30")
+wbs2$All_forms_completed_date <- as.Date(as.numeric(wbs2$All_forms_completed_date), origin = "1899-12-30")
+wbs2$Birth_date <- as.Date(as.numeric(wbs2$Birth_date), origin = "1899-12-30")
+
 # names(wbs2)[names(wbs2)=="Memory"] <- "AMIC"
 # names(wbs2)[names(wbs2)=="Memory_score"] <- "AMIC_score"
 # 
@@ -153,14 +161,28 @@ vars <- c("member_id", "gender", "dob", "wbs_survey_date",
           "marital", "educ", "living_status", "housing_type",
           "risk_score", "risk_level", "digital")
 
-# df1 <- df
-# wbs2t0 <- wbs2[wbs2$Questionnaire_code=="Q1",] # baseline only
-# df1 <- merge(df1, wbs2t0[, vars], # extract item matched by member ID
-#              by=c("member_id"), all.x = TRUE)
+# replace outdated full WBS data with latest high-risk WBS data
+wbs <- wbs[wbs$member_id %!in% wbs2$member_id, ] # keep members not found in latest high-risk WBS data
+wbs$All_forms_completed_date <- NA
+names(wbs2)[names(wbs2)=="Memory"] <- "AMIC"
+names(wbs2)[names(wbs2)=="Memory_score"] <- "AMIC_score"
 
-wbst0 <- wbs[wbs$Questionnaire_code=="Q1",] # baseline only
-df <- merge(df, wbst0[, vars], # extract item matched by member ID
+wbs2 <- wbs2[order(wbs2$wbs_survey_date),] # order by WBS survey date
+wbs2 <- distinct(wbs2, member_id, .keep_all = TRUE) # keep only first instance of same student & T1 (i.e. remove any repeats)
+
+df$member_id <- toupper(df$member_id)
+df <- merge(df, wbs2[, vars], # extract item matched by member ID
             by=c("member_id"), all.x = TRUE)
+
+# replace the few missing DOB from outdated full WBS data
+temp <- data.frame(member_id = wbs$member_id[wbs$member_id %in% df$member_id[is.na(df$dob) & df$member_id %in% wbs$member_id]], 
+                   dob = wbs$dob[wbs$member_id %in% df$member_id[is.na(df$dob) & df$member_id %in% wbs$member_id]])
+df <- merge(df, temp, # extract item matched by member ID
+            by=c("member_id"), all = TRUE)
+df$dob <- if_else(is.na(df$dob.x), df$dob.y, df$dob.x)
+df$dob.x <- NULL
+df$dob.y <- NULL
+rm(temp)
 
 df$age <- floor(interval(df$dob, df$ehealth_eval_timestamp) / duration(num = 1, units = "years")) # https://stackoverflow.com/a/27363833
 
@@ -173,6 +195,7 @@ df <- df[df$ehealth_eval_complete == 2,] # keep only completed records
 setwd(sprintf("~%s/ehealth", setpath))
 saveRDS(df, "ehealth_data.rds")
 saveRDS(wbs, "wbs_data.rds")
+
 # extract recording duration data ----
 # remotes::install_github("jmgirard/tidymedia")
 # install mediainfo & ffmpeg (ref: https://github.com/jmgirard/tidymedia/)
