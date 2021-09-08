29875354d5dd2513e8d72ddee85bdf24064a0329
King <king.wa.tam@gmail.com>
2021-09-08 15:45:22 +0800

    Create custom tableone function to show descriptive statistics and test results

--- t2dm/t2dm_analysis.R
+++ t2dm/t2dm_analysis.R
@@ -85,6 +85,9 @@ df$case_inc <- ifelse( (df$HbA1c >= 6.5 | df$FPG >= 7) & (df$Q35a != 1 & df$Q35 
 df$case_prev <- ifelse( (df$HbA1c >= 6.5 | df$FPG >= 7) & df$Q35 != 1, 1, 
                         ifelse(df$Q35 ==  1, 0, NA)) # prevalent cases as control
 
+df <- df[df$age <= 84,] # match age range of those who took health exam
+df$econ_active <- ifelse(df$econ == "Economically active persons", 1, 0)
+
 # recode income ----
 df$income <- factor(df$income, ordered = TRUE, levels = c("$0 No income",
                                                           "$1 - $1,999",
@@ -216,10 +219,244 @@ df$pa_recr_prop <- df$MET_recr/df$MET_week_con
 df$pa_trans_prop <- df$MET_trans/df$MET_week_con
 df$pa_work_trans_prop <- df$MET_work_trans/df$MET_week_con
 
-# exploratory analysis ----
-df <- df[df$age <= 84,] # match age range of those who took health exam
-df$econ_active <- ifelse(df$econ == "Economically active persons", 1, 0)
+# medical conditions ever diagnosed ----
+df$cancer <- ifelse(df$Q26a == 1, 1, 
+                    ifelse(df$Q26a == 2, 0, NA))
+df$stroke <- ifelse(df$Q26b == 1, 1, 
+                    ifelse(df$Q26b == 2, 0, NA))
+df$CHD <- ifelse(df$Q26c == 1, 1, 
+                    ifelse(df$Q26c == 2, 0, NA))
+df$asthma <- ifelse(df$Q26d == 1, 1, 
+                 ifelse(df$Q26d == 2, 0, NA))
+df$copd <- ifelse(df$Q26e == 1, 1, 
+                    ifelse(df$Q26e == 2, 0, NA))
+df$oth_respir <- ifelse(df$Q26f == 1, 1, 
+                  ifelse(df$Q26f == 2, 0, NA))
+df$hi_glucose <- ifelse(df$Q35 == 2, 1,
+                        ifelse(df$Q35 %in% c(1, 3), 0, NA))
+df$diabetes <- ifelse(df$Q35 == 1, 1,
+                        ifelse(df$Q35 %in% c(2, 3), 0, NA))
+df$hyperchol <- ifelse(df$Q29 == 1, 1, 
+                        ifelse(df$Q29 == 2, 0, NA))
+df$hypertension <- ifelse(df$Q32 == 1, 1, 
+                       ifelse(df$Q32 == 2, 0, NA))
+df$musculo  <- ifelse(df$Q27j == 1, 1, 
+                       ifelse(df$Q27j == 2, 0, NA))
+df$skin  <- ifelse(df$Q27l == 1, 1, 
+                      ifelse(df$Q27l == 2, 0, NA))
+df$ENT  <- ifelse(df$Q27c == 1, 1, 
+                   ifelse(df$Q27c == 2, 0, NA))
+df$thyroid  <- ifelse(df$Q27d == 1, 1, 
+                  ifelse(df$Q27d == 2, 0, NA))
+df$liver  <- ifelse(df$Q27f == 1, 1, 
+                      ifelse(df$Q27f == 2, 0, NA))
+df$anemia  <- ifelse(df$Q27h == 1, 1, 
+                    ifelse(df$Q27h == 2, 0, NA))
+df$stomach  <- ifelse(df$Q27g == 1, 1, 
+                     ifelse(df$Q27g == 2, 0, NA))
+df$kidney  <- ifelse(df$Q27e == 1, 1, 
+                      ifelse(df$Q27e == 2, 0, NA))
+df$blood  <- ifelse(df$Q27i == 1, 1, 
+                     ifelse(df$Q27i == 2, 0, NA))
+df$immune  <- ifelse(df$Q27k == 1, 1, 
+                    ifelse(df$Q27k == 2, 0, NA))
+df$parkinson  <- ifelse(df$Q27b == 1, 1, 
+                     ifelse(df$Q27b == 2, 0, NA))
+df$epilepsy  <- ifelse(df$Q27a == 1, 1, 
+                        ifelse(df$Q27a == 2, 0, NA))
+df$depression  <- ifelse(df$Q27m == 1, 1, 
+                       ifelse(df$Q27m == 2, 0, NA))
+df$anxiety  <- ifelse(df$Q27n == 1, 1, 
+                         ifelse(df$Q27n == 2, 0, NA))
+df$schizo  <- ifelse(df$Q27o == 1, 1, 
+                      ifelse(df$Q27o == 2, 0, NA))
+df$dementia  <- ifelse(df$Q27p == 1, 1, 
+                     ifelse(df$Q27p == 2, 0, NA))
+df$oth_chronic  <- ifelse(df$Q27qrs_regp == 1, 1, 
+                       ifelse(df$Q27qrs_regp == 2, 0, NA))
+df$eye  <- ifelse(df$Q39 == 1, 1, 
+                  ifelse(df$Q39 == 2, 0, NA))
 
+all_conditions <- c("cancer", "stroke", "CHD",
+                    "asthma", "copd", "oth_respir",
+                    "hi_glucose", "diabetes", "hyperchol", "hypertension", 
+                    "musculo", "skin", "ENT",
+                    "thyroid", "liver", "anemia",
+                    "stomach", "kidney", "blood",
+                    "immune", "parkinson", "epilepsy",
+                    "depression", "anxiety", "schizo",
+                    "dementia", "oth_chronic", "eye")
+mental_disorders <- c("depression", "anxiety", "schizo")
+comorbid_conditions  <-  c("cancer", "stroke", "CHD",
+                           "hi_glucose", "diabetes", "hyperchol", "hypertension",
+                           "kidney")
+
+df$any_disease <- ((rowSums(df[, all_conditions] == 1, na.rm=TRUE) > 0) * 1)
+df$any_comorbidity <- ((rowSums(df[, comorbid_conditions] == 1, na.rm=TRUE) > 0) * 1)
+
+df$mental_disorder <- ((rowSums(df[, mental_disorders] == 1, na.rm=TRUE) > 0) * 1)
+df$depression_anxiety <- ((rowSums(df[, mental_disorders[1:2]] == 1, na.rm=TRUE) > 0) * 1)
+
+df$case_allhealthy <- ifelse(is.na(df$Age_he), NA, 
+                          ifelse(df$any_disease == 0 & df$case_prev %in% c(NA, 0), 0, 
+                                 ifelse(df$case_prev == 1, 1, NA)))
+
+# df$case_healthy_unhealthy <- ifelse(is.na(df$Age_he), NA, 
+#                           ifelse(df$any_disease == 1 & df$case_prev %in% c(NA, 0), "unhealthy", 
+#                                  ifelse(df$any_disease == 0 & df$case_prev %in% c(NA, 0), "healthy",
+#                                         ifelse(df$case_prev == 1, "undiagnosed", NA))))
+
+df$case_healthy_comorbid <- ifelse(is.na(df$Age_he), NA,
+                                    ifelse(df$any_comorbidity == 1 & df$case_prev %in% c(NA, 0), "comorbid",
+                                           ifelse(df$any_comorbidity == 0 & df$case_prev %in% c(NA, 0), "healthy",
+                                                  ifelse(df$case_prev == 1, "undiagnosed", NA))))
+
+df$case_prev_healthy <- ifelse(df$case_prev %in% 1, "undiagnosed",
+                            ifelse(df$case_healthy_comorbid == "healthy", "healthy",
+                                                           ifelse(df$case_prev == 0, "prevalent DM", NA)))
+
+df$case_inc_healthy <- ifelse(df$case_inc %in% 1, "undiagnosed",
+                               ifelse(df$case_healthy_comorbid == "healthy", "healthy",
+                                      ifelse(df$case_inc == 0, "incident DM", NA)))
+
+df$case_comorbid <- ifelse(df$case_prev %in% 1, "undiagnosed",
+                               ifelse(df$case_healthy_comorbid == "comorbid", "comorbid", NA))
+
+# generate descriptive statistics ----
+gen_desc <- function(data, vars, ordinalVars, medianVars, group = "time", show_NA = FALSE, both_tests = FALSE){ 
+  categories <- unique(df[[group]]) 
+  if (!show_NA){
+    categories <- categories[categories %!in% NA]
+  }
+  
+  table <- data.frame(matrix(ncol = (2 + length(categories) + 1),  nrow = 0))
+  
+  row_count <- 1 
+  col_label <- 2
+  
+  colnames(table)[1] <- ""
+  colnames(table)[col_label] <- ""
+  
+  for (i in (1:length(categories))){
+    assign(paste0("col_", i), col_label + i)
+    table[row_count, get_("col_", i)] <- categories[i]
+  }
+  
+  table[row_count, 1] <- group
+  
+  col_pval <- col_label + length(categories) + 1
+  colnames(table) <- rep("", ncol(table))
+  colnames(table)[col_pval] <- "p-value"
+  
+  row_count <- row_count + 1
+  
+  table[row_count, 1] <- "N"
+  table[row_count, col_label] <- ""
+
+  
+  for (i in (1:length(categories))){
+    table[row_count, get_("col_", i)] <- length(data[[group]][data[[group]] %in% categories[i]])
+  }
+  
+  row_count <- row_count + 1
+  
+  get_mean_sd <- function(x){
+    mean <- mean(x, na.rm = TRUE) %>% round_format(decimal_places = 2)
+    sd <- sd(x, na.rm = TRUE) %>% round_format(decimal_places = 2)
+    return(paste0(mean, " (", sd, ")"))
+  }
+  
+  get_median_iqr <- function(x){
+    median <- median(x, na.rm = TRUE) %>% round_format(decimal_places = 0)
+    q1 <- quantile(x, probs =  0.25) %>% round_format(decimal_places = 0)
+    q3 <- quantile(x, probs =  0.75) %>% round_format(decimal_places = 0)
+    
+    return(paste0(median, " (", q1, "-", q3, ")"))
+  }
+  
+  for (var in vars){
+    print(var)
+    for (i in (1:length(categories))){
+      category <- categories[i]
+      
+      # if (class(df[[var]]) == "character") next()
+      # if (all(df[[var]][which(df[[group]] %in% category)] %in% NA)) next()
+      
+      if (var %in% ordinalVars){
+
+        table[row_count, 1] <- var
+        
+        p_value <- ifwarning(chisq.test(x = df[[group]], y = df[[var]])[['p.value']], chisq.test(x = df[[group]], y = df[[var]], simulate.p.value = TRUE, B=3000)[['p.value']])
+        if (p_value < 0.001){
+          table[row_count, col_pval] <- "<0.001"
+        } else {
+          table[row_count, col_pval] <- p_value %>% round(digits = 3)
+        }
+
+        row_count_ <- row_count # save count
+        total <- length(df[[var]][which(df[[group]] %in% category)])
+        
+        for (val in unique(df[[var]])[order(unique(df[[var]]))]){
+          table[row_count, col_label] <- val
+          
+          if (val %in% NA){
+            table[row_count, col_label] <- "N/A"
+          }
+          
+          n <-  length(df[[var]][which(df[[var]] %in% val & df[[group]] %in% category)])
+          
+          table[row_count, get_("col_", i)] <- paste0(n, " (", round_format(n/total*100, 1), "%)")
+          
+          if(n > 0) {row_count <- row_count + 1} 
+        }
+        
+        row_count__ <- row_count
+        row_count <- row_count_ # reset count
+      } 
+      
+      if (var %!in% ordinalVars){
+
+        table[row_count, 1] <- var
+        
+        anova.test <- aov(formula(paste0(var, " ~ ", group)),  data = df) %>% summary()
+        p_value <- anova.test[[1]][["Pr(>F)"]][[1]]
+        if (p_value < 0.001){
+          table[row_count, col_pval] <- "<0.001"
+        } else {
+          table[row_count, col_pval] <- p_value %>% round(digits = 3)
+        }
+        
+        table[row_count, col_label] <- "mean (sd)"
+        table[row_count, get_("col_", i)] <-  get_mean_sd(df[[var]][which(df[[group]] %in% category)])
+        
+      }
+      
+    }
+    
+    row_count <- iferror(row_count__, row_count + 1)
+    ifwarning(rm(row_count__), NA)
+  }
+  return(table)
+}
+
+allVars <- c("age", "age_cat", "sex", "educ", "marital", "econ", "econ_active", "occup", "income_num", "income_cat3", "district", "hse_income", "hse_type", 
+             # "live.alone", "Family.history.DM", 
+             "health", "who_pa", "checkup", "checkup_freq", "family_doctor", "BMI_recal")
+catVars <- c("age_cat", "sex", "educ", "marital", "econ", "occup", "income_cat3", "district", "hse_income", "hse_type", "checkup_freq", "HT_prev", "hyperchol_prev")
+
+allVars <- c(allVars , "MET_week_con", "MET_work", "MET_trans", "MET_recr", 
+             "pa_work_prop", "pa_trans_prop", "pa_recr_prop", 
+             "pa_work_vig", "pa_work_mod", "pa_trans", "pa_recr_vig", "pa_recr_mod", "pa_vig", "pa_mod", "sed_time_perday"
+             )
+
+df$case_inc_ <- ifelse(df$case_inc %in% 1, "undiagnosed",
+                       ifelse(df$case_inc %in% 0, "incident DM", NA))
+df$case_prev_ <- ifelse(df$case_prev %in% 1, "undiagnosed",
+                       ifelse(df$case_prev %in% 0, "prevalent DM", NA))
+
+gen_desc(df, vars = allVars, ordinalVars = catVars, medianVars = NULL, group = "case_healthy") %>% clipr::write_clip()
+
+# exploratory analysis ----
 table <- combine_tables(NULL, exponentiate = TRUE,
                glm(case_prev~1+age+male+educ+marital+econ_active+I(income_num/1000)+live.alone+Family.history.DM+health+checkup+I((pa_trans+pa_work_vig+pa_work_mod)>150), family = binomial, data =  df),
                glm(case_prev~1+age+male+educ+marital+econ_active+I(income_num/1000)+live.alone+Family.history.DM+health+checkup+pa_work_mod, family = binomial, data =  df),
@@ -346,7 +583,6 @@ catVars <- c("age_cat", "sex", "educ", "marital", "econ", "occup", "income_cat3"
 
 df$occup[!is.na(df$case_inc)]  <- drop_unused_value_labels(df$occup[!is.na(df$case_inc)] )
 
-
 allVars <- "occup"
 tableone::CreateTableOne(data =  df, strata = "case_inc", vars = allVars, factorVars = catVars) %>% 
   print(showAllLevels = TRUE) %>% clipr::write_clip()
@@ -355,5 +591,5 @@ allVars <- c("MET_week_con", "MET_work", "MET_trans", "MET_recr",
              "pa_work_prop", "pa_trans_prop", "pa_recr_prop", 
              "pa_work_vig", "pa_work_mod", "pa_trans", "pa_recr_vig", "pa_recr_mod", "pa_vig", "pa_mod", "sed_time_perday",
              "who_pa")
-tableone::CreateTableOne(data =  df, strata = "case_prev", vars = allVars) %>% 
+tableone::CreateTableOne(data =  df, strata = "case_inc", vars = allVars) %>% 
   print(showAllLevels = TRUE) %>% clipr::write_clip()
